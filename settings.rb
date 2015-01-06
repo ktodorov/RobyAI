48ff5240c783fcb8d4a92ae8c3ad9950078af009
ktodorov <kztodorov@outlook.com>
2015-01-06 23:48:05 +0200

    Добавен файл за настройките
     - Към момента, единствените нужни такива са за ActiveRecord

---
+++ settings.rb
@@ -0,0 +1,17 @@
+class Settings
+  
+  # ActiveRecord настройки към базата
+  gem 'activerecord-sqlserver-adapter', '~> 4.1.0'
+
+  require 'active_record'
+
+  ActiveRecord::Base.pluralize_table_names = false
+
+  ActiveRecord::Base.establish_connection(
+      :adapter => "sqlserver",
+      :host => "HOME-PC",
+      :database => "RobyAI_Prototype",
+      :username => "sa1",
+      :password => "sa"
+  )
+end
