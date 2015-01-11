aaba0b94a61836043289ed3e3a472255a4ea48d9
ktodorov <kztodorov@outlook.com>
2015-01-11 21:46:21 +0200

    Променен цвета на текста на Roby

--- IO/roby_io.rb
+++ IO/roby_io.rb
@@ -1,8 +1,8 @@
 module RobyIO
   def printn(input, *args)
-    puts "\n", input
+    puts "\n", "\033[36m#{input}\033[0m"
     args.each do |arg|
-      puts "\n", arg
+      puts "\n", "\033[36m#{arg}\033[0m"
     end
   end
 end
